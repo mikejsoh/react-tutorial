fd3523ccb649fc226c919dce69c636ba8473fe0a
Michael Oh <mike.js.oh@gmail.com>
2018-10-03 01:14:24 -0400

    Finished Section 3 Lecture 12
    - Added template and templateTwo
    - Added functions
    - Learned about conditional rendering

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,13 +1,81 @@
-"use strict";
+'use strict';
 
-console.log("App.js is running!");
+console.log('App.js is running!');
 
-// JSX - JavaScript XML
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is some info',
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+var user = {
+    name: 'Michael Oh',
+    age: 23,
+    location: 'London, ON'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location ? user.location : 'Unknown')
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,7 +1,43 @@
-console.log("App.js is running!");
+console.log('App.js is running!');
 
-// JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is some info',
+    options: ['One','Two']
+};
+
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle  && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var user = {
+    name: 'Michael Oh',
+    age: 23,
+    location: 'London, ON'
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>
+    }
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+        {getLocation(user.location ? user.location : 'Unknown')}
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
